9dd8555121734cdc4452bb4b5a6843e103b8ee4e
Edwin Vega <edwin.ponc@gmail.com>
2012-11-20 21:31:19 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeleApp::Application.config.secret_token = 'e666bd8467fdc8455a97d725fc152305742f99adefbcc3dd413f3a6773bcf1d4366995d6c904c2b4e758d716da6b15d8646011d0b6f7e0cb5a3ed531a81d8cb4'
