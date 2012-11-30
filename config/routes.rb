2deee30eb3403ed29a446b47ea5f86395137c099
Edwin Vega <edwin.ponc@gmail.com>
2012-11-29 20:21:04 -0500

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 TeleApp::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/help"
+
+  get "static_pages/about"
+  
+  get "static_pages/contact"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
